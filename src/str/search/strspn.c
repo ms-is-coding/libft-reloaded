b507852cf7af5298a9a7f86bc0567ba8f70ee797
M.S <ms@hadronlang.com>
2025-10-16 18:38:59 +0200

    feat(str): implement strcspn

--- src/str/search/strspn.c
+++ src/str/search/strspn.c
@@ -6,7 +6,7 @@
 /*   By: smamalig <smamalig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 17:58:36 by smamalig          #+#    #+#             */
-/*   Updated: 2025/10/07 14:40:25 by smamalig         ###   ########.fr       */
+/*   Updated: 2025/10/16 18:38:32 by smamalig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,16 @@ size_t	ft_strspn(const char *s, const char *accept)
 
 	len = 0;
 	while (s[len] && ft_strchr(accept, s[len]))
+		len++;
+	return (len);
+}
+
+size_t	ft_strcspn(const char *s, const char *reject)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len] && !ft_strchr(reject, s[len]))
 		len++;
 	return (len);
 }
